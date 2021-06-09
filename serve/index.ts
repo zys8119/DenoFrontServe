aa408964bbf4c3f6617aafa317e343041bc56dc0
zys8119 <770959294@qq.com>
2021-06-09 17:53:18 +0800

    初始化项目

--- serve/index.ts
+++ serve/index.ts
@@ -2,5 +2,24 @@ import { serve } from "https://deno.land/std@0.98.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-    req.respond({ body:"asdasd" });
+    console.log(req.url)
+    req.respond({
+        // body:new Uint8Array(await new Blob([JSON.stringify({
+        //     a:1,
+        //     b:2
+        // })]).arrayBuffer()),
+        body:new Uint8Array(await new Blob([`
+           <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Title</title>
+            </head>
+            <body>
+                    <div style="color: green">阿松大萨达</div>
+            </body>
+            </html>
+        `]).arrayBuffer()),
+    });
 }
+
